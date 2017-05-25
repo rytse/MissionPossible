785a6604742cfbb15dd719b723fae45635bfee54
Ryan Tse <rytse@mbhs.edu>
2017-05-25 18:33:25 -0400

    Remove sleep time between motor writes in motor.cpp

--- RPi/RPi/src/motor.cpp
+++ RPi/RPi/src/motor.cpp
@@ -7,12 +7,12 @@
 Motor::Motor(int _port) {
 	port = _port;
 	pwm.init(I2C_BUS, MC_I2C_ADDR);
-	sleep_secs(0.1);
+	//sleep_secs(0.1);
 	pwm.setPWMFreq(DEF_PWM_FREQ);
 }
 
 /*! Set the motor to a specific velocity in rotations per second */
 void Motor::setVel(double vel_rps) {
 	pwm.setPWM(port, vel_rps * PULSES_PER_RPS);
-	sleep_secs(0.01);
+	//sleep_secs(0.01);
 }
