fe58401ce7b5927df0613c47a99583f21c0c50ae
Ryan Tse <rytse@mbhs.edu>
2017-06-05 13:24:33 -0400

    Read other values from the synced IO stream

--- RPi/RPi/include/io.h
+++ RPi/RPi/include/io.h
@@ -5,6 +5,11 @@
 struct teleop_input {
 	double left_vel;
 	double right_vel;
+
+	bool turret_start;
+
+	bool arm_up_start;
+	bool arm_down_start;
 };
 
 void read_file(teleop_input pair);
--- RPi/RPi/src/io.cpp
+++ RPi/RPi/src/io.cpp
@@ -7,7 +7,15 @@ void read_file(teleop_input pair) {
 	ifstream _file = (IN_FILE_NAME);
 	string _line;
 
-	pair.left_vel = stod(getline(_file, _line));
-	pair.right_vel = stod(getline(_file, _line));
+	getline(_file, _line);
+	pair.left_vel = stod(_line);
+	getline(_file, _line);
+	pair.right_vel = stod(_line);
+	getline(_file, _line);
+	pair.turret_start = stoi(_line);
+	getline(_file, _line);
+	pair.arm_up_start = stoi(_line);
+	getline(_file, _line);
+	pair.arm_down_start = stoi(_line);
 	_file.close();
 }
