f00c15448a9771be9ff52c0ed6266c28089353ae
Noah Gleason <noah.gleason1@gmail.com>
2017-06-08 11:58:45 -0400

    Had to leave CS

--- RPi/RPi/include/main.h
+++ RPi/RPi/include/main.h
@@ -3,6 +3,9 @@
 /* Struct representing the robot's desired state
 (wheel speeds, currently activated probe, etc.)*/
 struct robot_state {
+  int temp_sensor_reading;
+  int left_light_reading;
+  int right_light_reading;
 };
 
 int main(void);
--- RPi/RPi/src/main.cpp
+++ RPi/RPi/src/main.cpp
@@ -1,4 +1,11 @@
 #include "../include/main.h"
+#include "../include/mcp3008Spi.h"
+
+const int TEMP_SENSOR_PORT = 0;
+const int LEFT_LIGHT_PORT = 1;
+const int RIGHT_LIGHT_PORT = 2;
+mcp3008Spi adc;
+
 
 int main(void) {
 	struct robot_state rs;
@@ -14,6 +21,7 @@ int main(void) {
 
 /* Initial setup before main loop runs */
 void setup(struct robot_state *rs) {
+	adc = ()
 }
 
 /* Post-program tear down */
